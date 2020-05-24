f70afe9df7170ef86cf46d029bf61e28e27f6c67
Ben Monro <benjamin.monro@walmartlabs.com>
2020-05-24 10:05:33 -0700

    fixup! Merge branch 'master' of github.com:testing-library/which-query

---
+++ src/__tests__/helpers/test-utils.js
@@ -0,0 +1,23 @@
+// yep copied from DTL
+import {getQueriesForElement} from '@testing-library/dom'
+
+function render(html, {container = document.createElement('div')} = {}) {
+  container.innerHTML = html
+  const containerQueries = getQueriesForElement(container)
+  function rerender(newHtml) {
+    return render(newHtml, {container})
+  }
+  return {container, rerender, ...containerQueries}
+}
+
+function renderIntoDocument(html) {
+  return render(html, {container: document.body})
+}
+
+function cleanup() {
+  document.body.innerHTML = ''
+}
+
+afterEach(cleanup)
+
+export {render, renderIntoDocument, cleanup}
