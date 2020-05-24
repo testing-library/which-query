84502695718be9a5e3677fc81cc168d40896ea3d
Ben Monro <benjamin.monro@walmartlabs.com>
2020-05-24 10:47:00 -0700

    fix: don't return 2nd param when not available

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const { getRoles } = require("@testing-library/dom");
 
+// eslint-disable-next-line consistent-return
 export function getSuggestedQuery({ element }) {
   const roles = getRoles(element);
 
   let queryName;
   const roleNames = Object.keys(roles);
+
   if (roleNames.length) {
     queryName = "ByRole";
     const [queryParam] = roleNames;
-    let queryOptions = null;
+
+    let queryOptions = "";
     if (element.textContent) {
       queryOptions = `, {name:/${element.textContent}/}`;
     }
 
     return `${queryName}("${queryParam}"${queryOptions})`;
   }
-
-  return null;
 }
